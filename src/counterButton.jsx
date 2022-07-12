add076cf3647cbacca5690f0a67707bac814250f
Devan Kavalchek <devankav7@gmail.com>
2022-07-11 23:59:10 -0400

    added functionality to button

--- src/counterButton.jsx
+++ src/counterButton.jsx
@@ -1,6 +1,12 @@
+import { useDispatch } from "react-redux"
+import { increment, decrement } from "./slice"
+
 function CounterButton(props) {
+    const dispatch = useDispatch()
+
     const onClick = () => {
         console.log("clicked!")
+        dispatch(increment())
     }
 
     return (<button onClick={onClick}>Click Me</button>)
